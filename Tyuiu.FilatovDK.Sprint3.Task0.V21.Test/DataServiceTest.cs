39a66cf598fad51ae5823f60e0b72eea6cb79be9
kiki769 <fiirve@bk.ru>
2024-10-29 15:43:04 +0500

    Task0

--- Tyuiu.FilatovDK.Sprint3.Task0.V21.Test/DataServiceTest.cs
+++ Tyuiu.FilatovDK.Sprint3.Task0.V21.Test/DataServiceTest.cs
@@ -8,11 +8,11 @@ namespace Tyuiu.FilatovDK.Sprint3.Task0.V21.Test
         public void ValidGetSumSeries()
         {
             DataService ds = new DataService();
-            double value = 5;
+            double value = 1.5;
             int stsrtValue = 1;
-            int stopValue = 10;
+            int stopValue = 13;
             double res = ds.GetSumSeries(value, stsrtValue, stopValue);
-            double wait = 12207030;
+            double wait = 580.932;
             Assert.AreEqual(wait, res);
         }
     }
