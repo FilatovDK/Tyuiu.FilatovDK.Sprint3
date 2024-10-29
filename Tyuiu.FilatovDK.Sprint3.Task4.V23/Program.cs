5a8d59c6c7ffdeb856d03b79c309ed2a06b3a062
kiki769 <fiirve@bk.ru>
2024-10-29 20:03:53 +0500

    Task4

---
+++ Tyuiu.FilatovDK.Sprint3.Task4.V23/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.FilatovDK.Sprint3.Task4.V23.Lib;
+namespace Tyuiu.FilatovDK.Sprint3.Task4.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue, stopValue;
+            Console.WriteLine("первое число = "); 
+            startValue = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("второе число = "); 
+            stopValue = Convert.ToInt16(Console.ReadLine());
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService DS = new DataService();
+            Console.WriteLine("результат: " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
