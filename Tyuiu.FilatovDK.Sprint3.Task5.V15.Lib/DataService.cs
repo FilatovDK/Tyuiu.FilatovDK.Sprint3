73292b93d126c6ed8883a88b42a2d1be7a90ef15
kiki769 <fiirve@bk.ru>
2024-10-29 21:14:27 +0500

    Task5

--- Tyuiu.FilatovDK.Sprint3.Task5.V15.Lib/DataService.cs
+++ Tyuiu.FilatovDK.Sprint3.Task5.V15.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.FilatovDK.Sprint3.Task5.V15.Lib
     {
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
-            double sumSeries = 1;
+            double sumSeries = 0;
             int i, k;
             for (i = startValue1; i <= stopValue1; i++)
             {
