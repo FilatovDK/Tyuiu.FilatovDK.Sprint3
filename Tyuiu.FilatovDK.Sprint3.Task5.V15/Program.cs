609a35df29559eaace36aa9f619ade51cd9a4324
kiki769 <fiirve@bk.ru>
2024-10-29 21:26:30 +0500

    Task5

--- Tyuiu.FilatovDK.Sprint3.Task5.V15/Program.cs
+++ Tyuiu.FilatovDK.Sprint3.Task5.V15/Program.cs
@@ -13,17 +13,15 @@ namespace Tyuiu.FilatovDK.Sprint3.Task5.V15
 
             int x = 5;
             int startValue1 = 1;
-            int stopValue1 = 3;
             int startValue2 = 1;
-            int stopValue2 = 10;
+            int stopValue1 = 3;
+            int stopValue2 = 13;
 
-            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Переменная Х = " + x);
             Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
             Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
             Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
             Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
-
-
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
@@ -32,7 +30,6 @@ namespace Tyuiu.FilatovDK.Sprint3.Task5.V15
             Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
 
             Console.ReadKey();
-
         }
     }
 }
